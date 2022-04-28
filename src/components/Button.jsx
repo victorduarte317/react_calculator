cf4ae231cc438f796fec8e52b87c3872289ce657
Victor Duarte <victorctii217@gmail.com>
2022-04-28 17:26:11 -0300

    Finished calculator style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,4 +2,14 @@ import React from 'react'
 import './Button.css'
 
 export default props => 
-    <button className='button'>{props.label}</button>
+    // Conditional class apply
+    <button 
+    onClick = {e => props.click && props.click(props.label)}
+    className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `}>
+        {props.label}
+    </button>
